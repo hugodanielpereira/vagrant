5f363f9d50045ad630035ef0635dc393f91f26ef
Hugo Daniel Pereira <37424599+hugodanielpereira@users.noreply.github.com>
2021-12-04 18:24:43 +0100

    End project Multi Machines and start Base Box

--- node_vagrant_multi/provisioners/install-mongo.sh
+++ node_vagrant_multi/provisioners/install-mongo.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 #get GPG key
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+apt-key list
 #create a sources list file
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
 #reload package database
 sudo apt-get update
 #install Mongo
