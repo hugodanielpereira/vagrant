f8d32ee9b5722f47ee2c8440071071dc8ab9b39c
Hugo Daniel Pereira <37424599+hugodanielpereira@users.noreply.github.com>
2021-12-04 18:29:22 +0100

    Creation of Project for Creation of Box

--- node_todolist/provisioners/install-mongo.sh
+++ node_todolist/provisioners/install-mongo.sh
@@ -5,7 +5,7 @@
 curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 apt-key list
 #create a sources list file
-# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 #reload package database
